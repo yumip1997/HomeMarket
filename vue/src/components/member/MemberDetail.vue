f94c9c948651900e9cfda752f480dbcf235cbfc3
yumi_park <yumip1997@gmail.com>
2021-02-23 22:35:16 +0900

    member detail기능

--- vue/src/components/member/MemberDetail.vue
+++ vue/src/components/member/MemberDetail.vue
@@ -1,5 +1,33 @@
 <template>
-  <div></div>
+  <div>
+    <div is="sui-divider" horizontal>
+      <h4 is="sui-header">
+        <i class="bar chart icon"></i>
+        Specifications
+      </h4>
+    </div>
+
+    <table class="ui definition table">
+      <tbody>
+        <tr>
+          <td class="two wide column">아이디</td>
+          <td>{{member.memberId}}</td>
+        </tr>
+        <tr>
+          <td>이름</td>
+          <td>{{member.name}}</td>
+        </tr>
+        <tr>
+          <td>이메일</td>
+          <td>{{member.email}}</td>
+        </tr>
+        <tr>
+          <td>주소</td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
