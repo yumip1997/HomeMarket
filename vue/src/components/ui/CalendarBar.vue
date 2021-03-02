fd5a76f5b75c68962639bb6b78dab5bd9f7186cb
yumi_park <yumip1997@gmail.com>
2021-03-03 00:16:43 +0900

    Add CalendarBar

---
+++ vue/src/components/ui/CalendarBar.vue
@@ -0,0 +1,38 @@
+<template>
+    <sui-grid-row>
+        <label class="block text-gray-600 text-sm font-bold mb-2" for="startDate">시작일</label>
+        <v-date-picker v-model="startDate">
+            <template v-slot="{ inputValue, inputEvents }">
+                <input
+                    class="bg-white border px-2 py-1 rounded"
+                    :value="inputValue"
+                    v-on="inputEvents"/>
+            </template>
+        </v-date-picker>
+
+        <label class="block text-gray-600 text-sm font-bold mb-2" for="endDate">종료일</label>
+        <v-date-picker v-model="endtDate">
+            <template v-slot="{ inputValue, inputEvents }">
+                <input
+                    class="bg-white border px-2 py-1 rounded"
+                    :value="inputValue"
+                    v-on="inputEvents"/>
+            </template>
+        </v-date-picker>
+    </sui-grid-row>
+</template>
+
+<script>
+    export default {
+        name: 'CalendarBar',
+        data: function () {
+            return {
+                startDate: new Date(), 
+                endDate: new Date()
+            }
+        }
+
+    }
+</script>
+
+<style></style>
--- vue/src/components/ui/index.js
+++ vue/src/components/ui/index.js
@@ -1,3 +1,4 @@
 export {default as MainMenu} from './MainMenu'
 export {default as SearchBar} from './SearchBar'
 export {default as SubmitButton} from './SubmitButton'
+export {default as CalendarBar} from './CalendarBar'
